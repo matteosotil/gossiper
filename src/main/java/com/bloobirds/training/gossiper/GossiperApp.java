f0781ca06101ef45f3cdc56064fa9ec6bb13ae88
MSB <matteo.sotil@gmail.com>
2020-02-29 10:25:48 +0100

    Removed unused variable.

--- src/main/java/com/bloobirds/training/gossiper/GossiperApp.java
+++ src/main/java/com/bloobirds/training/gossiper/GossiperApp.java
@@ -2,13 +2,12 @@ package com.bloobirds.training.gossiper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class GossiperApp {
 
-    public static void main(String[] args) {
-        ConfigurableApplicationContext run = SpringApplication.run(GossiperApp.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(GossiperApp.class, args);
+	}
 
 }
