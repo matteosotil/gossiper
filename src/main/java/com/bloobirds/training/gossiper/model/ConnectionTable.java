28d5388113eb964832eea03364462a54743b627c
MSB <matteo.sotil@gmail.com>
2020-02-29 11:26:42 +0100

    Added new method ConnectionTable.add

--- src/main/java/com/bloobirds/training/gossiper/model/ConnectionTable.java
+++ src/main/java/com/bloobirds/training/gossiper/model/ConnectionTable.java
@@ -12,44 +12,59 @@ import java.util.*;
 @EnableConfigurationProperties({GossiperConfigurationProperties.class})
 public class ConnectionTable {
 
-    private final Set<Connection> connections;
-    private final GossiperConfigurationProperties properties;
+	private final Set<Connection> connections;
+	private final GossiperConfigurationProperties properties;
 
-    public ConnectionTable(GossiperConfigurationProperties properties) {
-        this.properties = properties;
-        connections = Collections.synchronizedSet(new HashSet<>());
-        if (properties.getSeedHostname() != null && properties.getSeedName() != null) {
-            connections.addAll(Collections.singleton(new Connection(properties.getSeedName(), properties.getSeedHostname())));
-        }
-    }
+	public ConnectionTable(GossiperConfigurationProperties properties) {
+		this.properties = properties;
+		connections = Collections.synchronizedSet(new HashSet<>());
+		if (properties.getSeedHostname() != null && properties.getSeedName() != null) {
+			connections.addAll(Collections.singleton(new Connection(properties.getSeedName(), properties.getSeedHostname())));
+		}
+	}
 
-    public void addConnections(Collection<Connection> newConnections) {
-        newConnections.forEach(newConnection -> {
-            if (properties.getOwnName().equals(newConnection.getName())) {
-                return;
-            }
-            boolean success = connections.add(newConnection);
-            if (success) {
-                log.info("Node {} joined", newConnection.getName());
-            }
-        });
-    }
+	public void addConnections(Collection<Connection> newConnections) {
+		newConnections.forEach(newConnection -> {
+			if (properties.getOwnName().equals(newConnection.getName())) {
+				return;
+			}
+			boolean success = connections.add(newConnection);
+			if (success) {
+				log.info("Node {} joined", newConnection.getName());
+			}
+		});
+	}
 
-    public void remove(Connection connection) {
-        boolean success = connections.remove(connection);
-        if (success) {
-            log.info("Node {} left", connection.getName());
-        }
-    }
+	public void remove(Connection connection) {
+		boolean success = connections.remove(connection);
+		if (success) {
+			log.info("Node {} left", connection.getName());
+		}
+	}
 
-    public List<Connection> get(int n) {
-        List<Connection> copy = new ArrayList<>(connections);
-        Collections.shuffle(copy);
-        return copy.subList(0, Math.min(copy.size(), n));
-    }
+	public List<Connection> get(int n) {
+		List<Connection> copy = new ArrayList<>(connections);
+		Collections.shuffle(copy);
+		return copy.subList(0, Math.min(copy.size(), n));
+	}
 
-    public List<Connection> getAll() {
-        return new ArrayList<>(connections);
-    }
+	public List<Connection> getAll() {
+		return new ArrayList<>(connections);
+	}
+
+	/**
+	 * Adds a connection if its name is different that own name.
+	 * 
+	 * @param connection
+	 */
+	public void add(Connection connection) {
+		if (properties.getOwnName().equals(connection.getName())) {
+			return;
+		}
+		boolean success = connections.add(connection);
+		if (success) {
+			log.info("Node {} joined", connection.getName());
+		}
+	}
 
 }
