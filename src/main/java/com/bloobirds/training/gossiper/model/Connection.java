40f17e2c14b82c19eb39b590b126cc27ec864be9
MSB <matteo.sotil@gmail.com>
2020-03-01 20:48:38 +0100

    Connection must be serialized.

--- src/main/java/com/bloobirds/training/gossiper/model/Connection.java
+++ src/main/java/com/bloobirds/training/gossiper/model/Connection.java
@@ -1,12 +1,16 @@
 package com.bloobirds.training.gossiper.model;
 
+import java.io.Serializable;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Value;
 
 @Value
-public class Connection {
+public class Connection implements Serializable {
 
-    private final String name;
+	private static final long serialVersionUID = 3649772793236356335L;
+
+	private final String name;
     private final String hostname;
 
     public Connection(@JsonProperty("name") String name, @JsonProperty("hostname") String hostname) {
