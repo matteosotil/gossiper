a68c535eefdbc77124c5dbc3b2e470e3886af56a
MSB <matteo.sotil@gmail.com>
2020-03-01 21:30:14 +0100

    New interface for backup connection service.

---
+++ src/main/java/com/bloobirds/training/gossiper/backup/BackupConnectionTableService.java
@@ -0,0 +1,33 @@
+package com.bloobirds.training.gossiper.backup;
+
+import java.util.List;
+
+import com.bloobirds.training.gossiper.model.Connection;
+
+/**
+ * Backup service for connection table.
+ * 
+ * @author MatteoSotil
+ *
+ */
+public interface BackupConnectionTableService {
+
+	/**
+	 * Backups connection table.
+	 */
+	void write();
+
+	/**
+	 * Reads backup connection table.
+	 * 
+	 * @return
+	 */
+	List<Connection> read();
+
+	boolean isEnabled();
+
+	void clean();
+
+	boolean isBackupReady();
+
+}
