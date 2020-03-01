2b8ae0526758a0ae0c2ccbcc39e0ca4beb371c08
MSB <matteo.sotil@gmail.com>
2020-03-01 21:32:44 +0100

    New connection table initializer.

---
+++ src/main/java/com/bloobirds/training/gossiper/model/ConnectionTableInitializer.java
@@ -0,0 +1,34 @@
+package com.bloobirds.training.gossiper.model;
+
+import java.util.Collections;
+import javax.annotation.PostConstruct;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Service;
+
+import com.bloobirds.training.gossiper.GossiperConfigurationProperties;
+import com.bloobirds.training.gossiper.backup.BackupConnectionTableService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@EnableConfigurationProperties({ GossiperConfigurationProperties.class })
+public class ConnectionTableInitializer {
+
+	private final GossiperConfigurationProperties properties;
+	private final BackupConnectionTableService backupConnectionTable;
+	private final ConnectionTable connectionTable;
+
+	@PostConstruct
+	public void init() {
+		connectionTable.add(new Connection(properties.getOwnName(), properties.getHostName()));
+		if (properties.getSeedHostname() != null && properties.getSeedName() != null) {
+			connectionTable.addConnections(
+					Collections.singleton(new Connection(properties.getSeedName(), properties.getSeedHostname())));
+		}
+		if (backupConnectionTable.isBackupReady()) {
+			connectionTable.addConnections(backupConnectionTable.read());
+		}
+	}
+}
